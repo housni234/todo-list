a16a3f31c3c8828bd1f3a9f0283ef92b6562ced4
Housni <alami.housni@gmail.com>
2023-05-05 20:19:24 +0200

    rendering part1 done

---
+++ CS/projects/todo-list/src/App.js
@@ -0,0 +1,63 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault()
+      ;
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        {
+          id: crypto.randomUUID(),
+          title: newItem,
+          completed: false,
+        }
+      ]
+    })
+    setNewItem("")
+
+  }
+  function toggleTodo(id, completed) {
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit} className='new-item-form'>
+        <div className='form-row'>
+          <label htmlFor='item'>New Item</label>
+          <input
+            value={newItem}
+            onChange={e => setNewItem(e.target.value)}
+            type='text'
+            id='item' />
+          <button className='btn'>Add</button>
+        </div>
+      </form>
+      <h1 className='header'>Todo list</h1>
+      <ul className='list'>
+        {todos.map(todo => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type='checkbox'
+                  checked={todo.completed}
+                  onChange={e => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
+
+              </label>
+              <button className='btn-delete'>Delete</button>
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default App;
